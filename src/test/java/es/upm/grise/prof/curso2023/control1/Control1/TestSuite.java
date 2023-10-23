75e8de19f99adab88541dd227300c9868dfe8987
Lucas Martín <lucas.mgil-delgado@alumnos.upm.es>
2023-10-23 18:37:41 +0200

    Creada la clase TestSuite que permite ejecutar simultáneamente los tests de las clases Cliente y Pedido.

---
+++ src/test/java/es/upm/grise/prof/curso2023/control1/Control1/TestSuite.java
@@ -0,0 +1,9 @@
+package es.upm.grise.prof.curso2023.control1.Control1;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({PedidoTest.class, ClienteTest.class})
+public class TestSuite {
+}
