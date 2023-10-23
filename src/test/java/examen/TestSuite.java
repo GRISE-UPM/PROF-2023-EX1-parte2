5d0374b954c6a5974ce7c5e76a005a2162c22070
AlainSQ <alainsanchez.ep@gmail.com>
2023-10-23 19:05:10 +0200

    Suite creada

---
+++ src/test/java/examen/TestSuite.java
@@ -0,0 +1,10 @@
+package examen;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@SelectClasses({PedidoTest.class, ClienteTest.class})
+
+@Suite
+public class TestSuite {}
+
