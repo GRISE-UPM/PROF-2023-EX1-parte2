049ee26b29a0eac9bb51b9072565c10b50228765
andres <a.dossantos@alumnos.upm.es>
2023-10-23 18:38:24 +0200

    TestSuite

---
+++ src/test/java/es/upm/grise/examen/examen_a/AppTestSuite.java
@@ -0,0 +1,13 @@
+package es.upm.grise.examen.examen_a;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({AppTest.class})
+
+
+public class AppTestSuite {
+
+}
