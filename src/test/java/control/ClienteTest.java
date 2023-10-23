ea73f84234fc591b6e872fcc84f51c45e03dbcb7
100405923 <100405923@alumnos.uc3m.es>
2023-10-23 18:53:02 +0200

    Agregadas pruebas para Pedido y Cliente.

--- src/test/java/control/ClienteTest.java
+++ src/test/java/control/ClienteTest.java
@@ -2,6 +2,7 @@ package control;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
---
+++ src/test/java/control/TestSuite.java
@@ -0,0 +1,11 @@
+package control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.TestSuite;
+
+@TestSuite
+public class TestSuite {
+}
