cc5976c363ba2ecdb94d0238438a168748a12bd5
aloncrack7 <alonmadrid@gmail.com>
2023-10-23 19:03:48 +0200

    Add: Test suite

---
+++ src/test/java/es/upm/alumnos/alonso/TestSuite.java
@@ -0,0 +1,9 @@
+package es.upm.alumnos.alonso;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@SelectClasses({PedidoTest.class, ClienteTest.class})
+
+@Suite
+public class TestSuite {}
